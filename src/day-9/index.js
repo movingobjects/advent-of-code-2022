dc30622bfdb8efca1190e40b87f959184bf66472
Scott Thiessen <scott@movingobjects.io>
2022-12-09 07:52:48 -0800

    Clean up day 9

--- src/day-9/index.js
+++ src/day-9/index.js
@@ -17,71 +17,62 @@ const moves = getInput()
     dist: l.split(' ')[1]
   }));
 
-const sign = (n) => {
-  return (n > 0) ? 1 : ((n < 0) ? -1 : 0);
-}
+const getAns = (len) => {
 
+  let rope    = [],
+      tailXYs = [];
 
-const getAns = (ropeLen) => {
-
-  let rope = [];
-  let tailPosRecord = [];
-
-  times(ropeLen, () => rope.push([0, 0]));
+  times(len, () => rope.push({
+    x: 0,
+    y: 0
+  }));
 
   moves.forEach(({ dir, dist }) => {
 
-    const updateT = (index) => {
+    const updateKnot = (index) => {
 
-      const t = rope[index],
-            h = rope[index - 1];
+      const knot     = rope[index],
+            knotPrev = rope[index - 1];
 
-      if (index === 0) {
-        if (dir === 'U') t[1] -= 1;
-        if (dir === 'R') t[0] += 1;
-        if (dir === 'D') t[1] += 1;
-        if (dir === 'L') t[0] -= 1;
-        return;
-      }
+      if (!knotPrev) {
+        // Rope head
+        if (dir === 'U') knot.y -= 1;
+        if (dir === 'R') knot.x += 1;
+        if (dir === 'D') knot.y += 1;
+        if (dir === 'L') knot.x -= 1;
 
-      const distX = h[0] - t[0],
-            distY = h[1] - t[1];
-
-      if (h[0] === t[0]) {
-        if (Math.abs(distY) > 1) {
-          t[1] += sign(h[1] - t[1]);
-        }
-      } else if (h[1] === t[1]) {
-        if (Math.abs(distX) > 1) {
-          t[0] += sign(h[0] - t[0]);
-        }
       } else {
-        if (Math.abs(distX) > 1 || Math.abs(distY) > 1) {
-          t[0] += sign(h[0] - t[0]);
-          t[1] += sign(h[1] - t[1]);
+        // Not rope head
+        const distX  = knotPrev.x - knot.x,
+              distY  = knotPrev.y - knot.y,
+              isFarX = Math.abs(distX) > 1,
+              isFarY = Math.abs(distY) > 1;
+
+        if (isFarX || isFarY) {
+          knot.x += Math.sign(distX);
+          knot.y += Math.sign(distY);
         }
+
       }
 
     }
 
-    const recordTailPos = () => {
-      const [ x, y ] = rope.slice().pop();
-      tailPosRecord = uniq([
-        ...tailPosRecord,
+    const saveTailXY = () => {
+      const { x, y } = rope.slice().pop();
+      tailXYs = uniq([
+        ...tailXYs,
         `${x},${y}`
       ])
     }
 
-    times(dist, (i) => {
-      times(rope.length, (i) => {
-        updateT(i)
-      })
-      recordTailPos();
+    times(dist, () => {
+      times(rope.length, updateKnot)
+      saveTailXY();
     });
 
   })
 
-  return tailPosRecord.length;
+  return tailXYs.length;
 
 }
 
